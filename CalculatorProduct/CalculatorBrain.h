91d9393c8dbf42162a44e0fa37017c34d502683c
HaoQi Li <haoqili@gmail.com>
2012-11-12 22:33:15 -0800

    error on 1st function I put in

---
+++ CalculatorProduct/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  CalculatorProduct
+//
+//  Created by HaoQi on 11/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
---
+++ README.md
@@ -0,0 +1,1 @@
+First hw of 193p
